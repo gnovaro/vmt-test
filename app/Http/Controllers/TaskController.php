ca90d6f6cf557e716aca0e48f14b1dfb68d35f61
Gustavo Novaro <gnovaro@users.noreply.github.com>
2019-11-05 11:16:16 +0100

    Improve task controller api call

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -13,4 +13,33 @@ class TaskController extends MainController
         $data['tasks'] = $task->getAll();
         return view('task.index',$data);
     }
+    
+    public function add()
+    {
+        $task = new Task;
+        $data['task'] = $task;
+        return view('task.task',$data);
+    }
+    
+    public function edit(Request $request, int $id)
+    {
+        $task = Task::find($id);
+        $data['task'] = $task;
+        return view('task.task',$data);
+    }
+    
+    public save(Request $request)
+    {
+        //call to the api
+        $client = new Client();
+        $res = $client->request('POST', '/api/task', [
+            'form_params' => [
+                'name' => $request->name,
+                'due_date' => $request->due_date,
+                'description' => $request->description,                
+            ]
+        ]);
+        //@todo check status heare
+        return redirect('/task');
+    }
 }
