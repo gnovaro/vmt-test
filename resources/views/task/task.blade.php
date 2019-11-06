5df22e302eeeebe70ffee2c734b243834d7a7a6f
Gustavo Novaro <gnovaro@users.noreply.github.com>
2019-11-06 09:27:27 +0100

    Add values for edit

--- resources/views/task/task.blade.php
+++ resources/views/task/task.blade.php
@@ -5,15 +5,15 @@
     @csrf
     <div class="row">
         <label>{{ __('Name') }}</label>
-        <input type="text" name="name" required="required" class="from-control">
+        <input type="text" name="name" value="{{ $task->name }}" required="required" class="from-control">
     </div>
     <div class="row">
         <label>{{ __('Due date') }}</label>
-        <input type="date" name="due_date" required="required" class="from-control">
+        <input type="date" name="due_date" value="{{ $task->due_date }}" required="required" class="from-control">
     </div>
     <div class="row">
         <label>{{ __('Description') }}</label>
-        <textarea name="description" class="from-control"></textarea>
+        <textarea name="description" class="from-control">{{ $task->description }}</textarea>
     </div>
     <div class="row">
         <button type="submit" class="btn btn-primary">{{ __('Save') }}</button>
